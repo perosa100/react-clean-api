ae1b4ccde21f35eccd22cea914204c1e069f53cd
perosa100 <patrickperosapp@gmail.com>
2020-07-29 15:13:47 -0300

    feat: add login view

---
+++ src/main/index.tsx
@@ -0,0 +1,8 @@
+import Login from '@/presentation/pages/Login/login'
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+  <Login />,
+  document.getElementById('main')
+)
