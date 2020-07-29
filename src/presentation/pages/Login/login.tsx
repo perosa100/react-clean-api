1e57621d2c31fb7d3b93d87ada2fea56b920fb0d
perosa100 <patrickperosapp@gmail.com>
2020-07-29 15:54:26 -0300

    refactor: move input to a separate files

--- src/presentation/pages/Login/login.tsx
+++ src/presentation/pages/Login/login.tsx
@@ -1,24 +1,21 @@
+import Footer from '@/presentation/components/footer/footer'
+import Input from '@/presentation/components/input/input'
 import LoginHeader from '@/presentation/components/login-header/login-header'
 import Spinner from '@/presentation/components/spinner/spinner'
 import React from 'react'
 import Styles from './login-styles.scss'
-import Footer from '@/presentation/components/footer/footer'
+
 const Login: React.FC = () => {
   return <div className={Styles.login}>
+
     <LoginHeader />
 
     <form action="" className={Styles.form}>
+
       <h2>Login</h2>
-      <div className={Styles.inputWrap}>
-        <input type="email" name="email" placeholder="Digite seu e-mail" />
-        <span className={Styles.status}>🔴</span>
+      < Input type="email" name="email" placeholder="Digite seu e-mail" />
+      < Input type="password" name="password" placeholder="Digite sua senha" />
 
-      </div>
-
-      <div className={Styles.inputWrap}>
-        <input type="password" name="password" placeholder="Digite sua senha" />
-        <span className={Styles.status}>🔴</span>
-      </div>
       <button className={Styles.submit} type="submit">Entrar</button>
       <span className={Styles.link}>Cadastre-se</span>
       <div className={Styles.errorWrap}>
