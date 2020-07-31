747ae0bd19dec2ac423ecc944f4072e8f8ba9ece
perosa100 <patrickperosapp@gmail.com>
2020-07-30 23:52:18 -0300

    test: ensure login pressent spinner on submit

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -86,4 +86,16 @@ describe('LoginComponent', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
   })
+
+  test('Should show spinner on submit', () => {
+    const { sut } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } }) // valida em tempo real
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } }) // valida em tempo real
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = sut.getByTestId('spinner')
+    expect(spinner).toBeTruthy()
+  })
 })
