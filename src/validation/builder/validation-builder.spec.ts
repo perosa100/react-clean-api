2517b775405aede993514a78a429fa8a17d2a79a
perosa100 <patrickperosapp@gmail.com>
2020-07-31 19:10:32 -0300

    test: ensure ValidationBuilder return EmailFieldValidation

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -1,9 +1,14 @@
-import { RequireFieldValidation } from '@/validation/validators'
+import { EmailValidation, RequireFieldValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
     const validations = sut.field('any_field').required().build()
     expect(validations).toEqual([new RequireFieldValidation('any_field')])
+  })
+
+  test('Should return EmailFieldValidation', () => {
+    const validations = sut.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
   })
 })
