be5b3e844c119b90e4a8e799adf3ea9f42892c69
perosa100 <patrickperosapp@gmail.com>
2020-08-03 00:17:15 -0300

    test: ensure Input COmponent loads with readOnly

---
+++ src/presentation/components/input/input.spec.tsx
@@ -0,0 +1,15 @@
+import Context from '@/presentation/contexts/form/form-context'
+import { render } from '@testing-library/react'
+import React from 'react'
+import Input from './input'
+describe('Input Component', () => {
+  test('Should begin with readOnly', () => {
+    const { getByTestId } = render(
+      <Context.Provider value={{ state: {} }} >
+        <Input name="field" />
+      </Context.Provider>
+    )
+    const input = getByTestId('field') as HTMLInputElement
+    expect(input.readOnly).toBe(true)
+  })
+})
