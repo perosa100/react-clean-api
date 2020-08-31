337a9ac5419e4e8d55400063430d4b29576ab5c6
perosa100 <patrickperosapp@gmail.com>
2020-08-31 14:29:27 -0400

    feat: ensure local storage adapter

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,4 +1,5 @@
 import { makeRemoteAuthentication } from '@/main/usecases/authentication/remote-authentication-factory'
+import { makeLocalSaveAccessToken } from '@/main/usecases/save-access-token/save-access-token-factory'
 import { Login } from '@/presentation/pages'
 import React from 'react'
 import { makeLoginValidation } from './login-validation-factory'
@@ -6,6 +7,7 @@ import { makeLoginValidation } from './login-validation-factory'
 export const makeLogin: React.FC = () => {
   return (
     <Login
+      saveAccessToken={makeLocalSaveAccessToken()}
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
     />
