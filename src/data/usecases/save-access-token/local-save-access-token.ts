1fee02ac947ca225b1c9c0cffa40f395aeda19cd
perosa100 <patrickperosapp@gmail.com>
2020-09-05 00:20:14 -0400

    feat: ensure LocalSaveAccessToken throws no accessToken area provided

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
+import { UnexpectedError } from '@/domain/errors'
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
 export default class LocalSaveAccessToken implements SaveAccessToken {
-  constructor (private readonly setStorage: SetStorage) { }
+  constructor(private readonly setStorage: SetStorage) {}
 
-  async save (accessToken: string): Promise<void> {
+  async save(accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     await this.setStorage.set('accessToken', accessToken)
   }
 }
