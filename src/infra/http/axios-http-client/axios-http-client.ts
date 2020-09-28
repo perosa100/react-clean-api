6456180b7feef10de340f010958dda8e777ae385
perosa100 <patrickperosapp@gmail.com>
2020-09-28 14:20:57 -0300

    refactor: remove duplicate code

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -14,10 +14,7 @@ export class AxiosHttpClient implements HttpPostClient<any> {
     } catch (error) {
       axiosResponse = error.response
     }
-    return {
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data
-    }
+    return this.adapt(axiosResponse)
   }
 
   async get(params: HttpGetParams): Promise<HttpResponse> {
@@ -29,6 +26,10 @@ export class AxiosHttpClient implements HttpPostClient<any> {
       axiosResponse = error.response
     }
 
+    return this.adapt(axiosResponse)
+  }
+
+  private adapt(axiosResponse: AxiosResponse): HttpResponse {
     return {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
