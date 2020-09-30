b7eded7458c04fdc75d6b2ade08dd6713f5f0df5
perosa100 <patrickperosapp@gmail.com>
2020-09-30 09:34:06 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/load-list.tsx
+++ src/presentation/pages/survey-list/load-list.tsx
@@ -25,34 +25,8 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
 
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>08</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual seu Frameword preferido?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
-
-              <time>
-                <span className={Styles.day}>22</span>
-                <span className={Styles.month}>08</span>
-                <span className={Styles.year}>2020</span>
-              </time>
-
-              <p>Qual seu Frameword preferido?</p>
-            </div>
-            <footer>Ver Resultado</footer>
-          </li>
+          <li></li>
         </ul>
       </div>
       <Footer />
