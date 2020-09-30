b2498bc8f9de040c16a55e313b57e86171d395df
perosa100 <patrickperosapp@gmail.com>
2020-09-30 08:40:45 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/load-list.tsx
+++ src/presentation/pages/survey-list/load-list.tsx
@@ -25,6 +25,34 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver Resultado</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>08</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+
+              <p>Qual seu Frameword preferido?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>08</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+
+              <p>Qual seu Frameword preferido?</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
