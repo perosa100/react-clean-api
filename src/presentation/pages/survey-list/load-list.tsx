00678aaebc58bc9c91ccc51abe2c7ba7d8b69125
perosa100 <patrickperosapp@gmail.com>
2020-09-30 08:32:12 -0300

    refactor: move icon to its on file

--- src/presentation/pages/survey-list/load-list.tsx
+++ src/presentation/pages/survey-list/load-list.tsx
@@ -1,4 +1,4 @@
-import { Footer, Header } from '@/presentation/components'
+import { Footer, Header, Icon, IconName } from '@/presentation/components'
 import React from 'react'
 import Styles from './survey-list-styles.scss'
 
@@ -9,25 +9,18 @@ const SurveyList: React.FC = () => {
 
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
+
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <div
-                className={[Styles.iconWrap, Styles.green, Styles.red].join(
-                  ' '
-                )}
-              >
-                <img
-                  className={Styles.icon}
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII="
-                  alt=""
-                />
-              </div>
+              <Icon iconName={IconName.thumbUp} className={Styles.iconWrap} />
+
               <time>
                 <span className={Styles.day}>22</span>
                 <span className={Styles.month}>08</span>
                 <span className={Styles.year}>2020</span>
               </time>
+
               <p>Qual seu Frameword preferido?</p>
             </div>
             <footer>Ver Resultado</footer>
