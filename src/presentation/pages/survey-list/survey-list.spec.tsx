93c27d2a9b779dcc25d793d52ee171ec5d7ed516
perosa100 <patrickperosapp@gmail.com>
2020-10-01 14:12:13 -0300

    feat: ensure SurveyList Component  call LoadSurveyList one

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,11 +1,36 @@
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/usecases'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import SurveyList from './survey-list'
 
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+  return {
+    loadSurveyListSpy
+  }
+}
+
 describe('SurveyList Component', () => {
   test('should present 4 empty items on start', () => {
-    render(<SurveyList />)
+    makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
