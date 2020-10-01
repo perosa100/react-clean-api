a639ea9660d7ac78a4db54a7392f613c6f6bdc0c
perosa100 <patrickperosapp@gmail.com>
2020-10-01 14:39:23 -0300

    feat: ensure SurveyItem Component render with correct values

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,18 +1,22 @@
 import { SurveyModel } from '@/domain/models'
 import faker from 'faker'
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.random.uuid(),
+  question: faker.random.words(10),
+  answers: [
+    {
+      answer: faker.random.words(4),
+      image: faker.internet.url()
+    },
+    { answer: faker.random.words(5) }
+  ],
+  date: faker.date.recent(),
+  didAnswer: faker.random.boolean()
+})
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.random.uuid(),
-    question: faker.random.words(10),
-    answers: [
-      {
-        answer: faker.random.words(4),
-        image: faker.internet.url()
-      },
-      { answer: faker.random.words(5) }
-    ],
-    date: faker.date.recent(),
-    didAnswer: faker.random.boolean()
-  }
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel()
 ]
